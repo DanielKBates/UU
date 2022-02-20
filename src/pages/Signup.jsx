9ec47d1f8850540aa0d2bc2d8578fafcf37176dc
DanielKBates <danielbates95@gmail.com>
2022-02-20 00:17:28 -0500

    signup image

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -379,8 +379,8 @@ const Signup = () => {
           <div className="hidden lg:block relative w-0 flex-1">
             <img
               className="absolute inset-0 h-full w-full object-cover"
-              src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-              alt=""
+              src="/assets/images/altLogo2.png"
+              alt="Uptown Ultras"
             />
           </div>
         </div>
