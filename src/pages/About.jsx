d3d38c5a171bb1f572068d90b7c5ca6113bbccd0
DanielKBates <danielbates95@gmail.com>
2022-02-21 11:45:12 -0500

    .

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -30,7 +30,7 @@ const people = [
 
 function About() {
   useEffect(async () => {
-    const res = axios.get("https://uuexpress.herokuapp.com/api/test");
+    const res = axios.get("https://uuexpress.herokuapp.com/api/test/test-cookie");
     console.log(res);
   }, []);
   return (
