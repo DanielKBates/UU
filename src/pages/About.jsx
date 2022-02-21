4eaace7bd0b01d9e6b5dbac31cec86c246410b2b
DanielKBates <danielbates95@gmail.com>
2022-02-21 13:11:12 -0500

    get -> post, cookie in res, not setting to browser

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -30,7 +30,7 @@ const people = [
 
 function About() {
   useEffect(async () => {
-    const res = axios.get("https://uuexpress.herokuapp.com/api/test/test-cookie", { withCredentials: true});
+    const res = axios.post("https://uuexpress.herokuapp.com/api/test/test-cookie", { withCredentials: true});
     console.log(res);
   }, []);
   return (
