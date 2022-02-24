048e29ac1313a8eed2b06fe39b938a268072cf73
Daniel Bates <danielbates95@gmail.com>
2022-02-24 16:00:21 -0500

    .

--- src/components/FeatEvent.jsx
+++ src/components/FeatEvent.jsx
@@ -34,7 +34,7 @@ function FeatEvent() {
             className="m-auto cursor-pointer rounded-t-lg"
           />
           <div className="px-5 py-4 ">
-            <Link to={event.path}>
+            <Link to={`events${event.path}`}>
               <h2 className="cursor-pointer text-3xl text-blue-400 uppercase">
                 {event.title}
               </h2>
